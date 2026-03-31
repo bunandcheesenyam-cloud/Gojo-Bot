7fc591be95cec584225cab10bb89f6df601606af
bunandcheesenyam-cloud <bunandcheesenyam@gmail.com>
2026-03-30 21:10:08 -0500

    Update bot activity message

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Throughout Heaven and Earth, I alone am the One",
         // Activity type number (0 = Playing).
         type: 0, 
       },
